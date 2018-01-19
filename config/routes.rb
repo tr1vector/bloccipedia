a03238cb6273dc73aa7fd81c1ad6b6e82ef27b1a
tr1vector <tr1vector@outlook.com>
2018-01-19 08:24:00 -0700

    Bloccipedia - Adding Static Pages and Rspec for testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  get 'welcome/faq'
+
+  root 'welcome#index'
+  
 end
